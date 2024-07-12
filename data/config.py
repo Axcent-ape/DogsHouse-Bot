3ec87f469cd534a95b5d2e445da71542b7e96993
Axcent <97802272+Axcent-ape@users.noreply.github.com>
2024-07-12 13:39:56 +0300

    Add files via upload

---
+++ data/config.py
@@ -0,0 +1,30 @@
+# api id, hash
+API_ID = 1488
+API_HASH = 'abcde1488'
+
+REF_LINK = 'https://t.me/dogshouse_bot/join?startapp=RRQUZbFUQTGTu0N5hAueeg'
+
+DELAYS = {
+    'ACCOUNT': [5, 15],  # delay between connections to accounts (the more accounts, the longer the delay)
+}
+
+PROXY = {
+    "USE_PROXY_FROM_FILE": False,  # True - if use proxy from file, False - if use proxy from accounts.json
+    "PROXY_PATH": "data/proxy.txt",  # path to file proxy
+    "TYPE": {
+        "TG": "socks5",  # proxy type for tg client. "socks4", "socks5" and "http" are supported
+        "REQUESTS": "socks5"  # proxy type for requests. "http" for https and http proxys, "socks5" for socks5 proxy.
+        }
+}
+
+# session folder (do not change)
+WORKDIR = "sessions/"
+
+# timeout in seconds for checking accounts on valid
+TIMEOUT = 30
+
+SOFT_INFO = f"""{"Dogs 🦴".center(40)}
+Soft for https://t.me/dogshouse_bot; claim reward
+
+The soft also collects statistics on accounts and uses proxies from {f"the {PROXY['PROXY_PATH']} file" if PROXY['USE_PROXY_FROM_FILE'] else "the accounts.json file"}
+"""
